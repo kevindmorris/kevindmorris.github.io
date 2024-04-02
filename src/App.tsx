3db11ff9dd6218109f414068d796c36b3fe67387
kevindmorris <kdm0030@mix.wvu.edu>
2024-04-02 10:15:31 -0400

    Add theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import router from "@/router";
+import theme from "@/theme";
 import { ChakraProvider } from "@chakra-ui/react";
 import { RouterProvider } from "react-router-dom";
 
 export default function App() {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <RouterProvider router={router} />
     </ChakraProvider>
   );
