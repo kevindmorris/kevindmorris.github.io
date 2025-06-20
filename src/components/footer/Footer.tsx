0569e1825d2ab9db0a4788fd2c764e17a3a56638
Kevin Morris <132013488+kevindmorris@users.noreply.github.com>
2025-06-20 15:04:09 -0400

    Delete src directory

--- src/components/footer/Footer.tsx
+++
@@ -1,9 +0,0 @@
-import { Box } from "@chakra-ui/react";
-
-export default function Footer() {
-  return (
-    <Box as="footer" p={4} mt="auto">
-      <p>© 2024 Kevin Morris.</p>
-    </Box>
-  );
-}
--- src/main.tsx
+++
@@ -1,7 +0,0 @@
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
-import "@fontsource/gloria-hallelujah";
-import "@fontsource/gloria-hallelujah/400.css";
-
-ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
