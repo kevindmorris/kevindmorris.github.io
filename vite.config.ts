6c536ea799effed8102e2647987f3a5427a83769
Kevin Morris <132013488+kevindmorris@users.noreply.github.com>
2025-06-20 15:05:24 -0400

    Delete vite.config.ts

--- vite.config.ts
+++
@@ -1,9 +0,0 @@
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-  server: { port: 3000, open: true, strictPort: true },
-});
